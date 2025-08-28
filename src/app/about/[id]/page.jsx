b868a8a3f7833c5ea3e48dccf30ef0eef4769bd2
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-28 14:42:32 +0600

    dynamic blogs

--- src/app/about/[id]/page.jsx
+++ src/app/about/[id]/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BlogDetails = ({params}) => {  
     console.log(params)
     return (
-    <div>Blog Details for <span className='text-red-500 font-bold text-2xl'>{params.id}</span></div>
+    <div>Blog Details for <span className='text-red-500 font-bold text-2xl'>{params?.id}</span></div>
   )
 }
 
