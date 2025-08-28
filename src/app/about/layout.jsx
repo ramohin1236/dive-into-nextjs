afe486a422f6a80bb1857bb4021376c3be3d866e
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-28 12:52:07 +0600

    layout added

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const aboutLayout = ({ children }) => {
+  return (
+    <div className="flex h-screen gap-10 ">
+      <div className="w-1/4 border border-b-blue-700"><h1>Side bar</h1></div>
+      <div className="flex-1 border border-b-red-600">{children}</div>
+    </div>
+  );
+};
+
+export default aboutLayout;
